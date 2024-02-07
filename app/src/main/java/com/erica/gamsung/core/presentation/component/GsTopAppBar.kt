51332589465a23da62547164bfac33a07228816f
JJongmen <pjy90123@gmail.com>
2024-02-07 21:56:45 +0900

    refactor: GsTopAppBar 파라미터 기본값 수정 및 Preview 예제 추가

--- app/src/main/java/com/erica/gamsung/core/presentation/component/GsTopAppBar.kt
+++ app/src/main/java/com/erica/gamsung/core/presentation/component/GsTopAppBar.kt
@@ -1,5 +1,6 @@
 package com.erica.gamsung.core.presentation.component
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.rounded.AccountCircle
@@ -9,15 +10,16 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 @OptIn(ExperimentalMaterial3Api::class)
-@Preview
 @Composable
 fun GsTopAppBar(
-    title: String = "메인 페이지",
-    hasLeftIcon: Boolean = true,
-    hasRightIcon: Boolean = true,
+    modifier: Modifier = Modifier,
+    title: String,
+    hasLeftIcon: Boolean = false,
+    hasRightIcon: Boolean = false,
     onNavigationClick: () -> Unit = {},
     onActionsClick: () -> Unit = {},
 ) {
@@ -51,5 +53,16 @@ fun GsTopAppBar(
                 )
             }
         },
+        modifier = modifier,
     )
+}
+
+@Preview
+@Composable
+private fun GsTopAppBarPreview() {
+    Column {
+        GsTopAppBar(title = "마이 페이지")
+        GsTopAppBar(title = "메인 페이지", hasRightIcon = true)
+        GsTopAppBar(title = "글 선택 페이지", hasLeftIcon = true)
+    }
 }
