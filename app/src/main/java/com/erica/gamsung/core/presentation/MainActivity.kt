8ef9976410b443abd0f52ed32507d7773f0d2d49
JJongmen <pjy90123@gmail.com>
2024-02-13 01:13:08 +0900

    refactor: NavHost를 별도의 MainNavHost 컴포저블 함수 추출

--- app/src/main/java/com/erica/gamsung/core/presentation/MainActivity.kt
+++ app/src/main/java/com/erica/gamsung/core/presentation/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -25,29 +26,34 @@ class MainActivity : ComponentActivity() {
                 ) {
                     val navController = rememberNavController()
 
-                    NavHost(navController = navController, startDestination = "main") {
-                        composable(Screen.MAIN.route) { MainScreen(navController = navController) }
-                        composable(Screen.SETTING.route) { SettingScreen() }
-                        composable(Screen.PUBLISH_POSTING.route) { PublishPostingScreen() }
-                        composable(Screen.CHECK_POSTING.route) { CheckPostingScreen() }
-                    }
+                    MainNavHost(navController)
                 }
             }
         }
     }
+}
 
-    @Composable
-    private fun SettingScreen() {
-        Text(text = "SettingScreen") // TODO 구현 시작 시 제거 및 코드 이동
+@Composable
+fun MainNavHost(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = "main") {
+        composable(Screen.MAIN.route) { MainScreen(navController = navController) }
+        composable(Screen.SETTING.route) { SettingScreen() }
+        composable(Screen.PUBLISH_POSTING.route) { PublishPostingScreen() }
+        composable(Screen.CHECK_POSTING.route) { CheckPostingScreen() }
     }
+}
 
-    @Composable
-    private fun PublishPostingScreen() {
-        Text(text = "PublishPostingScreen") // TODO 구현 시작 시 제거 및 코드 이동
-    }
+@Composable
+fun SettingScreen() {
+    Text(text = "SettingScreen") // TODO 구현 시작 시 제거 및 코드 이동
+}
 
-    @Composable
-    private fun CheckPostingScreen() {
-        Text(text = "CheckPostingScreen") // TODO 구현 시작 시 제거 및 코드 이동
-    }
+@Composable
+fun PublishPostingScreen() {
+    Text(text = "PublishPostingScreen") // TODO 구현 시작 시 제거 및 코드 이동
+}
+
+@Composable
+fun CheckPostingScreen() {
+    Text(text = "CheckPostingScreen") // TODO 구현 시작 시 제거 및 코드 이동
 }
