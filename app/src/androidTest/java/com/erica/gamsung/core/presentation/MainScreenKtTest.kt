d9e3562e73a19afb14b32aa4fc36558191410ac8
JJongmen <pjy90123@gmail.com>
2024-02-13 01:13:29 +0900

    test: 클릭 시 네비게이션 작동 테스트

--- app/src/androidTest/java/com/erica/gamsung/core/presentation/MainScreenKtTest.kt
+++ app/src/androidTest/java/com/erica/gamsung/core/presentation/MainScreenKtTest.kt
@@ -1,9 +1,14 @@
 package com.erica.gamsung.core.presentation
 
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.ComposeContentTestRule
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.testing.TestNavHostController
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert.*
 import org.junit.Before
@@ -15,11 +20,14 @@ import org.junit.runner.RunWith
 class MainScreenKtTest {
     @get:Rule
     val rule: ComposeContentTestRule = createComposeRule()
+    lateinit var navController: TestNavHostController
 
     @Before
     fun setUp() {
         rule.setContent {
-            MainScreen()
+            navController = TestNavHostController(LocalContext.current)
+            navController.navigatorProvider.addNavigator(ComposeNavigator())
+            MainNavHost(navController = navController)
         }
     }
 
@@ -31,6 +39,16 @@ class MainScreenKtTest {
             .assertIsDisplayed()
     }
 
+    @Test
+    fun 설정_버튼을_누르면_설정_페이지로_이동한다() {
+        rule
+            .onNodeWithContentDescription("AccountCircle")
+            .performClick()
+
+        val route = navController.currentDestination?.route
+        assertEquals(route, Screen.SETTING.route)
+    }
+
     /** 메인 페이지에 글 발행 버튼이 화면에 보인다 */
     @Test
     fun publishButtonIsDisplayedOnMainPage() {
@@ -39,11 +57,32 @@ class MainScreenKtTest {
             .assertIsDisplayed()
     }
 
+
+    @Test
+    fun 글_발행_버튼을_누르면_글_발행_페이지로_이동한다() {
+        rule
+            .onNodeWithText("글 발행하러 가기")
+            .performClick()
+
+        val route = navController.currentDestination?.route
+        assertEquals(route, Screen.PUBLISH_POSTING.route)
+    }
+
     /** 메인 페이지에 발행 현황 확인 버튼이 화면에 보인다 */
     @Test
     fun checkPublishStatusButtonIsDisplayedOnMainPage() {
         rule
             .onNodeWithText("발행 현황 확인하기")
             .assertIsDisplayed()
+    }
+
+    @Test
+    fun 발행_현황_확인_버튼을_누르면_발행_현황_페이지로_이동한다() {
+        rule
+            .onNodeWithText("발행 현황 확인하기")
+            .performClick()
+
+        val route = navController.currentDestination?.route
+        assertEquals(route, Screen.CHECK_POSTING.route)
     }
 }
