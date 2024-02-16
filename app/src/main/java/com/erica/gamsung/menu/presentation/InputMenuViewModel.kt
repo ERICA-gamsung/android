01fe6f05b676f09a1878233db54a201c5266f86d
JJongmen <pjy90123@gmail.com>
2024-02-17 02:28:25 +0900

    refactor: 이벤트 발생 시 작동 로직 메서드 추출

--- app/src/main/java/com/erica/gamsung/menu/presentation/InputMenuViewModel.kt
+++ app/src/main/java/com/erica/gamsung/menu/presentation/InputMenuViewModel.kt
@@ -19,73 +19,78 @@ class InputMenuViewModel(
     private var _shouldNavigateState = MutableStateFlow(false)
     val shouldNavigateState = _shouldNavigateState.asStateFlow()
 
-    @Suppress("LongMethod")
     fun onEvent(event: InputMenuUiEvent) {
         when (event) {
-            is InputMenuUiEvent.NameChanged -> {
-                _inputMenuState.update {
-                    it.copy(
-                        name = event.name,
-                    )
-                }
+            is InputMenuUiEvent.NameChanged -> handleNameChanged(event)
+            is InputMenuUiEvent.PriceChanged -> handlePriceChanged(event)
+            InputMenuUiEvent.AddMenu -> handleAddMenu()
+            is InputMenuUiEvent.RemoveMenu -> handleRemoveMenu(event)
+            InputMenuUiEvent.SendMenus -> handleSendMenus()
+        }
+    }
+
+    private fun handleNameChanged(event: InputMenuUiEvent.NameChanged) {
+        _inputMenuState.update {
+            it.copy(
+                name = event.name,
+            )
+        }
+    }
+
+    private fun handlePriceChanged(event: InputMenuUiEvent.PriceChanged) {
+        _inputMenuState.update {
+            it.copy(
+                price = event.price,
+            )
+        }
+    }
+
+    private fun handleAddMenu() {
+        val isNameValid = _inputMenuState.value.name.isNotBlank()
+        val isPriceValid = _inputMenuState.value.price.isZeroOrPrimitiveInt()
+
+        if (isNameValid && isPriceValid) {
+            _menusState.update {
+                it.plusElement(Menu(_inputMenuState.value.name, _inputMenuState.value.price.toInt()))
             }
 
-            is InputMenuUiEvent.PriceChanged -> {
-                _inputMenuState.update {
-                    it.copy(
-                        price = event.price,
-                    )
-                }
+            _inputMenuState.update {
+                it.copy(
+                    name = "",
+                    price = "",
+                    isNameValid = true,
+                    isPriceValid = true,
+                )
             }
-
-            InputMenuUiEvent.AddMenu -> {
-                val isNameValid = _inputMenuState.value.name.isNotBlank()
-                val isPriceValid = _inputMenuState.value.price.isZeroOrPrimitiveInt()
-
-                if (isNameValid && isPriceValid) {
-                    _menusState.update {
-                        it.plusElement(Menu(_inputMenuState.value.name, _inputMenuState.value.price.toInt()))
-                    }
-
-                    _inputMenuState.update {
-                        it.copy(
-                            name = "",
-                            price = "",
-                            isNameValid = true,
-                            isPriceValid = true,
-                        )
-                    }
-                } else {
-                    _inputMenuState.update {
-                        it.copy(
-                            isNameValid = isNameValid,
-                            isPriceValid = isPriceValid,
-                        )
-                    }
-                }
+        } else {
+            _inputMenuState.update {
+                it.copy(
+                    isNameValid = isNameValid,
+                    isPriceValid = isPriceValid,
+                )
             }
+        }
+    }
 
-            is InputMenuUiEvent.RemoveMenu -> {
-                _menusState.update {
-                    it.filterIndexed { index, _ -> index != event.index }
-                }
+    private fun handleRemoveMenu(event: InputMenuUiEvent.RemoveMenu) {
+        _menusState.update {
+            it.filterIndexed { index, _ -> index != event.index }
+        }
+    }
+
+    private fun handleSendMenus() {
+        val menus = _menusState.value
+
+        if (menus.isEmpty()) {
+            _inputMenuState.update {
+                it.copy(
+                    isNameValid = false,
+                    isPriceValid = false,
+                )
             }
-
-            InputMenuUiEvent.SendMenus -> {
-                val menus = _menusState.value
-
-                if (menus.isEmpty()) {
-                    _inputMenuState.update {
-                        it.copy(
-                            isNameValid = false,
-                            isPriceValid = false,
-                        )
-                    }
-                } else {
-                    // TODO 서버로 메뉴 전송
-                    _shouldNavigateState.value = true
-                }
-            }
+        } else {
+            // TODO 서버로 메뉴 전송
+            _shouldNavigateState.value = true
         }
     }
 
