0baf1aff6f64158f5ab7a032fdecbe94e084927e
JJongmen <pjy90123@gmail.com>
2024-02-19 18:02:36 +0900

    feat: 영업 시간 UI 구현

--- app/src/main/java/com/erica/gamsung/store/presentation/InputStoreScreen.kt
+++ app/src/main/java/com/erica/gamsung/store/presentation/InputStoreScreen.kt
@@ -1,5 +1,6 @@
 package com.erica.gamsung.store.presentation
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -7,13 +8,29 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccessTime
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TimeInput
+import androidx.compose.material3.TimePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.erica.gamsung.core.presentation.Screen
@@ -23,7 +40,9 @@ import com.erica.gamsung.core.presentation.component.GsTopAppBar
 import com.erica.gamsung.core.presentation.component.InputTextBox
 import com.erica.gamsung.core.presentation.component.TextTitle
 import com.erica.gamsung.store.domain.StoreType
+import com.erica.gamsung.store.presentation.utils.toDisplayString
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun InputStoreScreen(navController: NavHostController = rememberNavController()) {
     Scaffold(
@@ -37,7 +56,12 @@ fun InputStoreScreen(navController: NavHostController = rememberNavController())
         ) {
             StoreNameSection(onValueChange = { /* TODO */ }, isValid = true)
             StoreTypeSection(onClick = { /* TODO */ }, selectedStoreType = StoreType.CAFE)
-            StoreBusinessHoursSection()
+            StoreBusinessHoursSection(
+                openTimePickerState = TimePickerState(0, 0, false),
+                closeTimePickerState = null,
+                onOpenTimeUpdate = { /* TODO */ },
+                onCloseTimeUpdate = { /* TODO */ },
+            )
             StoreBusinessDaysSeciton()
             StoreAddressSection()
             StorePhoneNumberSection()
@@ -85,8 +109,87 @@ private fun StoreTypeSection(
 }
 
 @Composable
-fun StoreBusinessHoursSection() {
-    Text(text = "StoreBusinessHoursSection") // TODO
+@OptIn(ExperimentalMaterial3Api::class)
+private fun StoreBusinessHoursSection(
+    openTimePickerState: TimePickerState?,
+    closeTimePickerState: TimePickerState?,
+    onOpenTimeUpdate: (TimePickerState) -> Unit,
+    onCloseTimeUpdate: (TimePickerState) -> Unit,
+) {
+    TextTitle(
+        title = "영업 시간",
+        isRequired = true,
+        description = null,
+        modifier = Modifier.padding(top = 10.dp),
+    )
+
+    Row {
+        HoursSection(
+            timePickerState = openTimePickerState,
+            onUpdate = onOpenTimeUpdate,
+            modifier = Modifier.weight(1f),
+            title = "시작시간",
+        )
+        HoursSection(
+            timePickerState = closeTimePickerState,
+            onUpdate = onCloseTimeUpdate,
+            modifier = Modifier.weight(1f),
+            title = "종료시간",
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun HoursSection(
+    title: String,
+    timePickerState: TimePickerState?,
+    onUpdate: (TimePickerState) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    var showTimePicker by remember { mutableStateOf(false) }
+    val currentTimePickerState = remember { timePickerState ?: TimePickerState(0, 0, false) }
+
+    if (showTimePicker) {
+        Dialog(
+            onDismissRequest = {
+                onUpdate(currentTimePickerState)
+                showTimePicker = false
+            },
+        ) {
+            TimeInput(state = currentTimePickerState)
+        }
+    }
+
+    Column(
+        modifier = modifier.padding(end = 40.dp),
+    ) {
+        TextTitle(
+            title = title,
+            isRequired = false,
+            description = null,
+            modifier = Modifier.padding(top = 10.dp),
+        )
+        TextButton(
+            onClick = {
+                showTimePicker = true
+            },
+            colors = ButtonDefaults.textButtonColors(contentColor = Color.Gray),
+            shape = RoundedCornerShape(10.dp),
+            border = BorderStroke(1.dp, Color.Gray),
+        ) {
+            Icon(
+                imageVector = Icons.Default.AccessTime,
+                contentDescription = title,
+                modifier = Modifier.padding(end = 10.dp),
+            )
+            Text(
+                text = timePickerState?.toDisplayString() ?: "선택 안함",
+                style = MaterialTheme.typography.bodyLarge,
+            )
+            Spacer(modifier = Modifier.weight(1f))
+        }
+    }
 }
 
 @Composable
