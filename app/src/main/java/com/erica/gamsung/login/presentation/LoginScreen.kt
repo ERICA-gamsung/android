4bfb63af5d68b3d37a0ab156f67f2cf058f3d47d
JJongmen <pjy90123@gmail.com>
2024-03-09 23:27:46 +0900

    feat: 로그인 화면의 TitleSection 구현

--- app/src/main/java/com/erica/gamsung/login/presentation/LoginScreen.kt
+++ app/src/main/java/com/erica/gamsung/login/presentation/LoginScreen.kt
@@ -7,12 +7,20 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.erica.gamsung.core.presentation.theme.VividBlue
 
 @Composable
 fun LoginScreen() {
@@ -42,6 +50,24 @@ private fun TitleSection(modifier: Modifier = Modifier) {
     Column(
         modifier = modifier,
     ) {
+        Text(
+            text =
+                buildAnnotatedString {
+                    withStyle(style = SpanStyle()) {
+                        appendLine("반가워요!")
+                        appendLine("오늘부터 가게 마케팅은")
+                    }
+                    withStyle(SpanStyle(color = VividBlue)) {
+                        append("감성과")
+                    }
+                    withStyle(SpanStyle()) {
+                        append(" 함께 해요!")
+                    }
+                },
+            style = MaterialTheme.typography.headlineSmall,
+            fontWeight = FontWeight.Bold,
+            lineHeight = 40.sp,
+        )
     }
 }
 
