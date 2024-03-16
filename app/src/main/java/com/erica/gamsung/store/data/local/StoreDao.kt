9b56e903494902f4a641e135355f414945d8a121
JJongmen <pjy90123@gmail.com>
2024-03-17 01:13:37 +0900

    feat: local DB에서 가게 정보 가져오기

--- app/src/main/java/com/erica/gamsung/store/data/local/StoreDao.kt
+++ app/src/main/java/com/erica/gamsung/store/data/local/StoreDao.kt
@@ -1,10 +1,14 @@
 package com.erica.gamsung.store.data.local
 
 import androidx.room.Dao
+import androidx.room.Query
 import androidx.room.Upsert
 
 @Dao
 interface StoreDao {
     @Upsert
     fun upsert(store: StoreEntity)
+
+    @Query("SELECT * FROM stores WHERE id == :storeId")
+    fun getStoreById(storeId: Long): StoreEntity?
 }
