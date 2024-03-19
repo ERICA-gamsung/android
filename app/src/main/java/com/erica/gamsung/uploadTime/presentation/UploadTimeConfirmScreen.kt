9ce5281f9127364e00a5b409f3a3ee3d345f811f
johnhih619 <jaewonheo666@gmail.com>
2024-03-19 19:29:58 +0900

    Refactor: code Review 고려하여 리팩터링 진행

--- app/src/main/java/com/erica/gamsung/uploadTime/presentation/UploadTimeConfirmScreen.kt
+++ app/src/main/java/com/erica/gamsung/uploadTime/presentation/UploadTimeConfirmScreen.kt
@@ -3,6 +3,7 @@ package com.erica.gamsung.uploadTime.presentation
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -16,18 +17,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 
-@Suppress("MagicNumber", "LongMethod")
 @Preview
 @Composable
 fun UploadTimeConfirmScreen(
     // navController: NavHostController = rememberNavController(),
-    viewModel: CalendarViewModel = viewModel(),
 ) {
-    val isSelected = false
     // val scheduledDataMap = viewModel.scheduleDataMap
     Scaffold {
         Column(
@@ -38,79 +36,85 @@ fun UploadTimeConfirmScreen(
             verticalArrangement = Arrangement.SpaceEvenly,
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Box(
-                modifier =
-                    Modifier
-                        .fillMaxWidth()
-                        .weight(1f),
-            )
+            Spacer(modifier = Modifier.weight(1f))
+            ContentSection(modifier = Modifier.weight(2f))
+            Spacer(modifier = Modifier.weight(1f))
+        }
+    }
+}
 
-            Column(
-                modifier = Modifier.weight(1f),
-                verticalArrangement = Arrangement.SpaceEvenly,
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                Column(
-                    verticalArrangement = Arrangement.SpaceEvenly,
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Column(
-                        verticalArrangement = Arrangement.SpaceEvenly,
-                        modifier = Modifier.weight(1f),
-                    ) {
-                        Text(
-                            "글이 발행되고 있습니다",
-                            style = MaterialTheme.typography.titleLarge,
-                            fontWeight = FontWeight.Bold,
-                        )
-                        Text(
-                            "발행 현황을 확인해 보세요!",
-                            style = MaterialTheme.typography.titleLarge,
-                            fontWeight = FontWeight.Bold,
-                        )
-                    }
-                    Box(
-                        modifier = Modifier.weight(1f),
-                        contentAlignment = Alignment.Center,
-                    ) {
-                        Button(
-                            modifier = Modifier.fillMaxWidth(0.5f),
-                            shape = RoundedCornerShape(12.dp),
-                            colors =
-                                ButtonDefaults.buttonColors(
-                                    containerColor =
-                                        if (isSelected) {
-                                            MaterialTheme.colorScheme.secondary
-                                        } else {
-                                            MaterialTheme
-                                                .colorScheme
-                                                .primary
-                                        },
-                                    contentColor =
-                                        if (!isSelected) {
-                                            MaterialTheme.colorScheme.onPrimary
-                                        } else {
-                                            MaterialTheme
-                                                .colorScheme
-                                                .onSurface
-                                        },
-                                ),
-                            onClick = {
-                                !isSelected
-                                // TODO
-                            },
-                        ) {
-                            Text(text = "메인으로")
-                        }
-                    }
-                }
-            }
-            Box(
-                modifier =
-                    Modifier
-                        .fillMaxWidth()
-                        .weight(1f),
-            )
+@Composable
+private fun ContentSection(modifier: Modifier) {
+    Column(
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxWidth(),
+    ) {
+        TextSection(Modifier.weight(1f))
+        ActionButton(Modifier.weight(1f))
+    }
+}
+
+@Composable
+private fun TextSection(modifier: Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.SpaceEvenly,
+    ) {
+        Spacer(modifier = Modifier.weight(1f))
+        Text(
+            modifier = Modifier.weight(1f),
+            text = "글이 발행되고 있습니다",
+            style = MaterialTheme.typography.titleLarge,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+        )
+        Text(
+            modifier = Modifier.weight(1f),
+            text = "발행 현황을 확인해 보세요!",
+            style = MaterialTheme.typography.titleLarge,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+        )
+    }
+}
+
+@Suppress("MagicNumber")
+@Composable
+private fun ActionButton(modifier: Modifier) {
+    val isSelected = false
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center,
+    ) {
+        Button(
+            modifier = Modifier.fillMaxWidth(0.5f),
+            shape = RoundedCornerShape(12.dp),
+            colors =
+                ButtonDefaults.buttonColors(
+                    containerColor =
+                        if (isSelected) {
+                            MaterialTheme.colorScheme.secondary
+                        } else {
+                            MaterialTheme
+                                .colorScheme
+                                .primary
+                        },
+                    contentColor =
+                        if (!isSelected) {
+                            MaterialTheme.colorScheme.onPrimary
+                        } else {
+                            MaterialTheme
+                                .colorScheme
+                                .onSurface
+                        },
+                ),
+            onClick = {
+                !isSelected
+                // TODO
+            },
+        ) {
+            Text(text = "메인으로")
         }
     }
 }
