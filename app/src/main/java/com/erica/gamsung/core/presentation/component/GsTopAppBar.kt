fbf343ea3e9e8e7a4345acf86c84edf79f2f22b7
johnhih619 <jaewonheo666@gmail.com>
2024-03-25 20:59:50 +0900

    Fix: 공용 탑바 글씨체 수정

--- app/src/main/java/com/erica/gamsung/core/presentation/component/GsTopAppBar.kt
+++ app/src/main/java/com/erica/gamsung/core/presentation/component/GsTopAppBar.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -24,7 +25,12 @@ fun GsTopAppBar(
     onActionsClick: () -> Unit = {},
 ) {
     CenterAlignedTopAppBar(
-        title = { Text(text = title) },
+        title = {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.labelLarge,
+            )
+        },
         navigationIcon = {
             // 왼쪽 아이콘
             if (hasLeftIcon) {
