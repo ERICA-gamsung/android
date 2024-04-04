e28c789e05156e2e0ec0ffeb3d92925eb5ded9ac
johnhih619 <jaewonheo666@gmail.com>
2024-04-04 21:00:05 +0900

    Feature: 서버와의 통신 가정한 목업 데이터 작성

--- app/src/main/java/com/erica/gamsung/post/data/mock/MockData.kt
+++ app/src/main/java/com/erica/gamsung/post/data/mock/MockData.kt
@@ -3,6 +3,40 @@ package com.erica.gamsung.post.data.mock
 import com.erica.gamsung.post.domain.Schedule
 import com.erica.gamsung.post.domain.ScheduleList
 
+val mockPosts =
+    listOf(
+        Post(
+            id = 1,
+            content =
+                """Discover the beauty of our products and enjoy the latest technology.
+            |Discover the beauty of our products and enjoy the latest technology.
+            |Discover the beauty of our products and enjoy the latest technology.
+            |Discover the beauty of our products and enjoy the latest technology.
+            |Discover the beauty of our products and enjoy the latest technology.
+                """.trimMargin(),
+        ),
+        Post(
+            id = 2,
+            content =
+                """Learn how we're making a difference with sustainability at the heart of our products.
+            |Learn how we're making a difference with sustainability at the heart of our products.
+            |Learn how we're making a difference with sustainability at the heart of our products.
+            |Learn how we're making a difference with sustainability at the heart of our products.
+            |Learn how we're making a difference with sustainability at the heart of our products.
+                """.trimMargin(),
+        ),
+        Post(
+            id = 3,
+            content =
+                """
+            |Join our community events and connect with other Gamsung enthusiasts.
+            |Join our community events and connect with other Gamsung enthusiasts
+            |Join our community events and connect with other Gamsung enthusiasts.
+            |Join our community events and connect with other Gamsung enthusiasts.
+            |
+                """.trimMargin(),
+        ),
+    )
 val mockSchedules =
     ScheduleList(
         schedules =
---
+++ app/src/main/java/com/erica/gamsung/post/data/mock/Post.kt
@@ -0,0 +1,6 @@
+package com.erica.gamsung.post.data.mock
+
+data class Post(
+    val id: Int,
+    val content: String,
+)
