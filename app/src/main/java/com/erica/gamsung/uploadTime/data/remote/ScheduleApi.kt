dc46068d7e6c23e000c35cc0e6c6df6137775614
JJongmen <pjy90123@gmail.com>
2024-05-31 00:13:45 +0900

    feat: 스케줄 업로드 API 액세스 토큰 함께 보내도록 수정

--- app/src/main/java/com/erica/gamsung/uploadTime/data/remote/ScheduleApi.kt
+++ app/src/main/java/com/erica/gamsung/uploadTime/data/remote/ScheduleApi.kt
@@ -3,11 +3,13 @@ package com.erica.gamsung.uploadTime.data.remote
 import com.erica.gamsung.uploadTime.domain.ScheduleDataModel
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface ScheduleApi {
     @POST("postings/option")
     suspend fun uploadSchedules(
         @Body scheduleDataList: List<ScheduleDataModel>,
+        @Header("Authorization") bearerToken: String,
     ): Response<List<PostScheduleDataResponseModel>>
 }
